9af14e2ae0350fce7a97ad302939e94c34468999
Richard Lau <riclau@uk.ibm.com>
2016-11-02 13:45:13 -0400

    Convert testcases to use tap
    PR-URL: https://github.com/nodejs/nodereport/pull/15
    Reviewed-By:
    Richard Chamberlain <richard_chamberlain@uk.ibm.com>
    Sam Roberts <vieuxtech@gmail.com>
    Ben Noordhuis <info@bnoordhuis.nl>

---
+++ test/test-exception.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Testcase to produce NodeReport on uncaught exception
+if (process.argv[2] === 'child') {
+  require('../').setEvents('exception');
+
+  function myException(request, response) {
+    const m = '*** exception.js: testcase exception thrown from myException()';
+    throw new UserException(m);
+  }
+
+  function UserException(message) {
+    this.message = message;
+    this.name = 'UserException';
+  }
+
+  myException();
+} else {
+  const common = require('./common.js');
+  const spawn = require('child_process').spawn;
+  const tap = require('tap');
+
+  const child = spawn(process.execPath, [__filename, 'child']);
+  child.on('exit', (code, signal) => {
+    const expectedExitCode = process.platform === 'win32' ? 0xC0000005 : null;
+    const expectedSignal = process.platform === 'win32' ? null : 'SIGILL';
+    tap.plan(4);
+    tap.equal(code, expectedExitCode, 'Process should not exit cleanly');
+    tap.equal(signal, expectedSignal,
+              'Process should exit with expected signal ');
+    const reports = common.findReports(child.pid);
+    tap.equal(reports.length, 1, 'Found reports ' + reports);
+    const report = reports[0];
+    common.validate(tap, report, child.pid);
+  });
+}
