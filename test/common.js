5f287046c7ecfcea32d977061778d004c39a6328
Richard Lau <riclau@uk.ibm.com>
2016-11-02 13:45:13 -0400

    Convert testcases to use tap

---
+++ test/common.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const fs = require('fs');
+
+const REPORT_SECTIONS = [
+  'NodeReport',
+  'JavaScript Stack Trace',
+  'JavaScript Heap',
+  'System Information'
+];
+
+exports.findReports = (pid) => {
+  // Default filenames are of the form NodeReport.<date>.<time>.<pid>.<seq>.txt
+  const format = '^NodeReport\\.\\d+\\.\\d+\\.' + pid + '\\.\\d+\\.txt$';
+  const filePattern = new RegExp(format);
+  const files = fs.readdirSync('.');
+  return files.filter((file) => filePattern.test(file));
+};
+
+exports.validate = (t, report, pid) => {
+  t.test('Validating ' + report, (t) => {
+    fs.readFile(report, (err, data) => {
+      const reportContents = data.toString();
+      const plan = REPORT_SECTIONS.length + (pid ? 1 : 0);
+      t.plan(plan);
+      if (pid) {
+        t.match(reportContents, new RegExp('Process ID: ' + pid),
+                'Checking report contains expected process ID ' + pid);
+      }
+      REPORT_SECTIONS.forEach((section) => {
+        t.match(reportContents, new RegExp('==== ' + section),
+                'Checking report contains ' + section + ' section');
+      });
+    });
+  });
+};
