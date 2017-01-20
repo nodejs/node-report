richardlau/nodereport

Fix source directory for install target

binding.gyp
@@ -25,7 +25,7 @@
       "copies": [
         {
           "destination": "<(module_root_dir)",
-          "files": ["<(module_root_dir)/build/Release/nodereport.node"]
+          "files": ["<(PRODUCT_DIR)/nodereport.node"]
         }]
     },
   ],

nodejs/node-report

binding.gyp
@@ -31,7 +31,7 @@
       "copies": [
         {
           "destination": "<(module_root_dir)",
-          "files": ["<(module_root_dir)/build/Release/api.node"]
+          "files": ["<(PRODUCT_DIR)/api.node"]
         }]
     },
   ],