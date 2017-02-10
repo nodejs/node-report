2bba89d1560c0ea139125fbb8bd3cd957c7ba74e
Richard Lau <riclau@uk.ibm.com>
2017-02-10 13:47:38 -0500

    test: fix test-api-getreport.js
    process.argv0 was added in Node.js v6.4.0 and doesn't exist in Node.js v4.

--- test/test-api-getreport.js
+++ test/test-api-getreport.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// Testcase for returning NodeReport as a string via API call
+// Testcase for returning report as a string via API call
 
 const common = require('./common.js');
 const tap = require('tap');
 const nodereport = require('../');
 var report_str = nodereport.getReport();
 common.validateContent(report_str, tap, {pid: process.pid,
-  commandline: [process.argv0, 'test/test-api-getreport.js'].join(' ')
+  commandline: [process.argv[0], 'test/test-api-getreport.js'].join(' ')
 });
