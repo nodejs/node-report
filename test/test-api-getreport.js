f170edfd88e31eb690bfd56bd25b94a59707d61a
Richard Lau <riclau@uk.ibm.com>
2017-02-10 13:47:38 -0500

    test: fix test-api-getreport.js
    process.argv0 was added in Node.js v6.4.0 and doesn't exist in Node.js v4.
    
    PR-URL: #58
    Reviewed-By: Richard Chamberlain <richard_chamberlain@uk.ibm.com>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>

--- test/test-api-getreport.js
+++ test/test-api-getreport.js
@@ -1,11 +1,25 @@
 'use strict';
 
-// Testcase for returning NodeReport as a string via API call
+// Testcase for returning report as a string via API call
 
-const common = require('./common.js');
-const tap = require('tap');
-const nodereport = require('../');
-var report_str = nodereport.getReport();
-common.validateContent(report_str, tap, {pid: process.pid,
-  commandline: [process.argv0, 'test/test-api-getreport.js'].join(' ')
-});
+if (process.argv[2] === 'child') {
+  const nodereport = require('../');
+  console.log(nodereport.getReport());
+} else {
+  const common = require('./common.js');
+  const spawnSync = require('child_process').spawnSync;
+  const tap = require('tap');
+
+  const args = [__filename, 'child'];
+  const child = spawnSync(process.execPath, args);
+  tap.plan(3);
+  tap.strictSame(child.stderr.toString(), '',
+                 'Checking no messages on stderr');
+  const reportFiles = common.findReports(child.pid);
+  tap.same(reportFiles, [], 'Checking no report files were written');
+  tap.test('Validating report content', (t) => {
+    common.validateContent(child.stdout, t, { pid: child.pid,
+      commandline: process.execPath + ' ' + args.join(' ')
+    });
+  });
+}
