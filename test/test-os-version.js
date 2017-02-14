854f86ba46c7027d52c5123e293a76e0c43b4220
Richard Lau <riclau@uk.ibm.com>
2017-02-14 12:30:01 -0500

    Drop redundant if test. Wrap long lines.

--- test/test-os-version.js
+++ test/test-os-version.js
@@ -17,11 +17,17 @@ const os_name = os_map[os.platform()];
 const report_str = nodereport.getReport();
 const version_str = report_str.match(/OS version: .*(?:\r*\n)/);
 if (common.isWindows()) {
-  tap.match(version_str, new RegExp('OS version: ' + os_name), 'Checking OS version');
+  tap.match(version_str,
+            new RegExp('OS version: ' + os_name), 'Checking OS version');
 } else if (common.isAIX() && !os.release().includes('.')) {
-  // For Node.js prior to os.release() fix for AIX:  https://github.com/nodejs/node/pull/10245
-  tap.match(version_str, new RegExp('OS version: ' + os_name + ' \\d+.' + os.release()), 'Checking OS version');
-} else if (!common.isWindows()) {
-  tap.match(version_str, new RegExp('OS version: ' + os_name + ' .*' + os.release()), 'Checking OS version');
+  // For Node.js prior to os.release() fix for AIX:
+  // https://github.com/nodejs/node/pull/10245
+  tap.match(version_str,
+            new RegExp('OS version: ' + os_name + ' \\d+.' + os.release()),
+            'Checking OS version');
+} else {
+  tap.match(version_str,
+            new RegExp('OS version: ' + os_name + ' .*' + os.release()),
+            'Checking OS version');
 }
 
